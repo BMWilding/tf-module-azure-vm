26420ab125afe5a383c2f273610b17971a02866e
Rory Chatterton <rorychatt@users.noreply.github.com>
2018-02-04 19:53:40 +1100

    Update __outputs.tf

--- __outputs.tf
+++ __outputs.tf
@@ -1,8 +1,6 @@
 #########################################
 # IP Outputs
 #########################################
-output public_ips  { value = ["${azurerm_public_ip.vm_pi.*.ip_address}"] }
 output public_dns  { value = ["${azurerm_public_ip.vm_pi.*.fqdn}"] }
-output private_mac_address { value = ["${azurerm_network_interface.ni.*.mac_address}"] }
 output private_ips { value = ["${azurerm_network_interface.ni.*.private_ip_address}"] }
 output private_dns { value = ["${azurerm_network_interface.ni.*.internal_fqdn}"] }
